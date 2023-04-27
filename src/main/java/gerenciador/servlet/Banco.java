3742b3281d591035feeaa484416e04f6a3275faf
lzmattana <l.z.mattana@gmail.com>
2023-04-27 11:09:20 -0300

    listando empresa com servlet

--- src/main/java/gerenciador/servlet/Banco.java
+++ src/main/java/gerenciador/servlet/Banco.java
@@ -7,6 +7,17 @@ public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>(); // lista para guardar empresas
 	
+	// bloco estatico carregado ao iniciar MV
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("ORACLE");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("GOOGLE");
+		lista.add(empresa);
+		lista.add(empresa2);
+
+	}
+	
 	public void adiciona(Empresa empresa) {		
 		lista.add(empresa);
 	}
---
+++ src/main/java/gerenciador/servlet/ListaEmpresasServlet.java
@@ -0,0 +1,37 @@
+package gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListaEmpresasServlet
+ */
+@WebServlet("/listaEmpresas")
+public class ListaEmpresasServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		Banco banco = new Banco(); // iniciando obj 
+		List<Empresa> lista = banco.getEmpresas(); //recuperando lista de empresas
+		
+		//resposta html
+		PrintWriter out = response.getWriter();		
+		out.println("<html><body>");
+        out.println("<ul>");
+        for (Empresa empresa : lista) { 
+            out.println("<li>" + empresa.getNome() + "</li>");
+        }        
+        out.println("</ul>");
+        out.println("</body></html>");
+	}
+
+}
