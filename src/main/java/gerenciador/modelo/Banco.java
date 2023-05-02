ed3967d918459e50ef561009f94d324aa9fc4d29
lzmattana <l.z.mattana@gmail.com>
2023-05-02 17:52:00 -0300

    tela de login

--- src/main/java/gerenciador/modelo/Banco.java
+++ src/main/java/gerenciador/modelo/Banco.java
@@ -5,10 +5,11 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
-	
+
 	private static List<Empresa> lista = new ArrayList<>(); // lista para guardar empresas
-	private static Integer chaveSequencial = 1; // simulando cad de id 
-	
+	private static Integer chaveSequencial = 1; // simulando cad de id
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
+
 	// bloco estatico carregado ao iniciar MV
 	static {
 		Empresa empresa = new Empresa();
@@ -20,34 +21,44 @@ public class Banco {
 		lista.add(empresa);
 		lista.add(empresa2);
 
+		Usuario u1 = new Usuario();
+		u1.setLogin("leo");
+		u1.setSenha("123");
+
+		Usuario u2 = new Usuario();
+		u2.setLogin("vivi");
+		u2.setSenha("123");
+
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
+
 	}
-	
-	public void adiciona(Empresa empresa) {		
+
+	public void adiciona(Empresa empresa) {
 		empresa.setId(Banco.chaveSequencial++);
 		lista.add(empresa);
 	}
-	
+
 	public List<Empresa> getEmpresas() {
 		return Banco.lista;
 	}
 
 	public void removeEmpresa(Integer id) {
-		//resolvendo exceção
-		// usando iterator 
+		// resolvendo exceção
+		// usando iterator
 		Iterator<Empresa> it = lista.iterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			Empresa emp = it.next();
-			if(emp.getId() == id) {
+			if (emp.getId() == id) {
 				it.remove();
 			}
 		}
 
-		
 	}
 
 	public Empresa buscaEmpresaPorId(Integer id) {
 		for (Empresa empresa : lista) {
-			if(empresa.getId() == id) {
+			if (empresa.getId() == id) {
 				return empresa;
 			}
 		}
