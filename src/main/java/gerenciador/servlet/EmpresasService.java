d7bfcfc377bf167a12999ccb0583621e12d010d3
lzmattana <l.z.mattana@gmail.com>
2023-05-04 14:10:55 -0300

    add XML

--- src/main/java/gerenciador/servlet/EmpresasService.java
+++ src/main/java/gerenciador/servlet/EmpresasService.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import gerenciador.modelo.Banco;
 import gerenciador.modelo.Empresa;
@@ -18,17 +18,30 @@ import gerenciador.modelo.Empresa;
 public class EmpresasService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		List<Empresa> empresas = new Banco().getEmpresas();
-		
-		//obj gson 
-		Gson gson = new Gson();
-		String json = gson.toJson(empresas); 
-		
+
+		// obj xstream
+		XStream xstream = new XStream();
+		xstream.alias("empresa", Empresa.class);
+		// usando a biblio p transf em xml
+		String xml = xstream.toXML(empresas); 
+
 		// definindo qual tipo de conteudo app/json boa pratica
-		response.setContentType("application/json");
-		//metodo para imprimir json
-		response.getWriter().print(json);
+		response.setContentType("application/xml");
+		// metodo para imprimir json
+		response.getWriter().print(xml);
+		
+//		// obj gson
+//		Gson gson = new Gson();
+//		// usando a biblio p transf em json
+//		String json = gson.toJson(empresas);
+//
+//		// definindo qual tipo de conteudo app/json boa pratica
+//		response.setContentType("application/json");
+//		// metodo para imprimir json
+//		response.getWriter().print(json);
 	}
 
 }
